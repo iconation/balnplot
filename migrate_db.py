8979bf8163e164f4512a3464387780e1a9e4b312
ICONation <iconation.team@gmail.com>
2021-05-05 23:35:27 +0200

    Add balnSicxPrice to GUI

--- migrate_db.py
+++ migrate_db.py
@@ -1,52 +1,48 @@
 import sqlite3
 import time
 import ciso8601
+import sys
 
 def get_table(c, table_name) -> list:
-    now = int(time.time())
     c.execute(f"SELECT * FROM {table_name}");
     return c.fetchall()
 
 def get_all_data(c) -> dict:
     tables = [
-      "totalBalnSupply",
-      "stakedBalnSupply",
       "balnBnusdPrice",
       "sicxBnusdPrice",
-      "sicxIcxPool",
-      "sicxBnusdPool",
-      "balnBnusdPool",
-      "balnBnusdApy",
-      "sicxBnusdApy",
-      "sicxIcxApy",
-      "loansApy"
     ]
     result = {}
     for table in tables:
         result[table] = get_table(c, table)
     return result
 
-conn = sqlite3.connect('./baln_old.db')
-data = get_all_data(conn.cursor())
-
-for name, table in data.items():
-    newtable = []
-    for entry in table:
-        new_entry = list(entry)
-        new_entry[0] = int(time.mktime(ciso8601.parse_datetime(new_entry[0]).timetuple()))
-        newtable.append(tuple(new_entry))
-    data[name] = newtable
-conn.close()
-
-# Export to new
 conn = sqlite3.connect('./baln.db')
 c = conn.cursor()
+data = get_all_data(conn.cursor())
 
-for name, table in data.items():
-    for entry in table:
-        values = ','.join(map(str, entry))
-        query = f"INSERT INTO {name} VALUES ({values})"
-        c.execute(query)
+if len(data['sicxBnusdPrice']) != len(data['balnBnusdPrice']):
+    print("Error, invalid tables")
+    sys.exit(0)
+
+length = len(data['sicxBnusdPrice'])
+
+balnsicxprices = []
+for i in range(length):
+    balnbnusd = data['balnBnusdPrice'][i]
+    sicxbnusd = data['sicxBnusdPrice'][i]
+    if abs(int(sicxbnusd[0]) - int(balnbnusd[0])) > 2:
+        print(f"Error, unsync at {i} : {sicxbnusd[0]}, {balnbnusd[0]}")
+        sys.exit(0)
+    
+    balnsicxprices.append(float(balnbnusd[1] / sicxbnusd[1]))
+
+# Export to new
+for i in range(length):
+    balnbnusd = data['balnBnusdPrice'][i]
+    entry = [balnbnusd[0], balnsicxprices[i]]
+    values = ','.join(map(str, entry))
+    c.execute(f"INSERT INTO balnSicxPrice VALUES ({values})")
 
 conn.commit()
 conn.close()
